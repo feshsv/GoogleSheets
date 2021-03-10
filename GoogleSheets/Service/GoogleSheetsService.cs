cd8e2d60f462ea084d6ef49acf4b5770258a204d
SergeiFeshchukov <s.feshchukov@modulbank.ru>
2021-03-10 12:10:21 +0300

    переименовал классы

---
+++ GoogleSheets/Service/GoogleSheetsService.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Util.Store;
+
+namespace GoogleSheets.Service
+{
+    internal static class GoogleSheetsService
+    {
+        private static readonly string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
+        private const string ApplicationName = "Google Sheets API.NET";
+        private static readonly SheetsService Service = GetGoogleService();
+
+        
+        /// <summary>
+        /// Получает данные из таблицы GoogleSheets в виде списка списков обьектов object, которые представляют данные таблицы.
+        /// </summary>
+        /// <param name="sheetUrl">АДРЕС GOOGLE ТАБЛИЦЫ</param>
+        /// <param name="startRow">ДИАПАЗОН ИЗ КОТОРОГО НУЖНО ПОЛУЧИТЬ ДАННЫЕ</param>
+        /// <returns>Список списков объектов object, которые представляют данные таблицы</returns>
+        public static IList<IList<object>> GetValueList(string sheetUrl, string startRow)
+        {
+            var values = GetValues(sheetUrl, startRow);
+            if ("1".Equals(startRow))
+            {
+                return values;
+            }
+
+            var title = GetTitle(sheetUrl);
+
+            foreach (var value in values)
+            {
+                title.Add(value);
+            }
+            return title;
+        }
+        
+        
+        /// <summary>
+        /// Авторизация в Google таблицах. Тут есть обращение к полученному от гугла credentials.json (читай readme)
+        /// При первой успешной авторизации автоматически сохраняется файл token.json. В последующем авторизация не требуется.
+        /// </summary>
+        /// <returns>GoogleSheetsService - сервис работы с гугл таблицами, необходимый для доступа к ним.</returns>
+        private static SheetsService GetGoogleService()
+        {
+            UserCredential credential;
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            { 
+                const string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+            }
+            var service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+            return service;
+        }
+        
+        
+        /// <summary>
+        /// Получает первую строку. В ней как правило названия столбцов.
+        /// </summary>
+        /// <param name="sheetUrl"></param>
+        /// <returns>Список списков объектов object, которые представляют данные таблицы</returns>
+        private static IList<IList<object>> GetTitle(string sheetUrl)
+        {
+            var requestTitle = Service.Spreadsheets.Values.Get(sheetUrl, "A1:ZZZ1");
+            var responseTitle = requestTitle.Execute();
+            var titleList = responseTitle.Values;
+            return titleList;
+        }
+
+
+        /// <summary>
+        /// Получает все данные заданного диапазона.
+        /// </summary>
+        /// <param name="sheetUrl"></param>
+        /// <param name="start"></param>
+        /// <returns>Список списков объектов object, которые представляют данные таблицы</returns>
+        private static IList<IList<object>> GetValues(string sheetUrl, string start)
+        {
+            var rangeData = "A" + start + ":ZZZ"; // Если нужна вся таблица то "A1:ZZZ"
+            var request = Service.Spreadsheets.Values.Get(sheetUrl, rangeData);
+            var response = request.Execute();
+            var values = response.Values;
+            return values;
+        }
+    }
+}
---
+++ GoogleSheets/Service/QueryStringsUtils.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GoogleSheets.Models;
+
+namespace GoogleSheets.Service
+{
+    internal static class QueryStringsUtils
+    {
+        /// <summary>
+        /// Генерит insert query для загрузки в SQL
+        /// </summary>
+        /// <param name="sheetValues">данные таблицы</param>
+        /// <returns>QueryStrings - команды SQL для загрузки данныех в БД</returns>
+        public static QueryStrings GetQuery(IList<IList<object>> sheetValues)
+        {
+            var titles = sheetValues[0];
+            sheetValues.RemoveAt(0);
+
+            var createTableQuery = GetCreateTableQueryString(titles);
+            var queryList = GetInsertDataQueryString(titles, sheetValues);
+            return new QueryStrings(createTableQuery, queryList);
+        }
+
+
+        /// <summary>
+        /// Метод подготавливает Query строку запроса в SQL для создания таблицы
+        /// </summary>
+        /// <param name="titles">первая строка в excel. обычно там названия столбцов.</param>
+        /// <returns>Строка создания таблицы в БД</returns>
+        private static string GetCreateTableQueryString(IEnumerable<object> titles)
+        {
+            var createTableQuery =
+                titles.Aggregate("CREATE TABLE DataFromGoogleSheet(", (current, title) => current + ("[" + title + "]" + " NVARCHAR(MAX), "));
+
+            createTableQuery = createTableQuery.Trim(',', ' ');
+            createTableQuery += ")";
+
+            return createTableQuery;
+        }
+
+
+        /// <summary>
+        /// Метод собирает в список SQL INSERT QUERY строки по 1000 строк и в конце суёт остатки. MS SQL больше 1000 за раз не ест.
+        /// </summary>
+        /// <param name="titles">первая строка в excel. обычно там названия столбцов.</param>
+        /// <param name="sheetValues">все оставшиеся строки с данными</param>
+        /// <returns>Строки вставки данных в таблицу БД</returns>
+        private static List<string> GetInsertDataQueryString(IList<object> titles, IList<IList<object>> sheetValues)
+        {
+            var eachThousandRows = new List<string>();
+            var count = 0;
+            var createTableQuery = "INSERT INTO DataFromGoogleSheet(";
+
+            while (sheetValues.Count > 0)
+            {
+                if (count > 999)
+                {
+                    createTableQuery = createTableQuery.Trim(',', '(');
+                    eachThousandRows.Add(createTableQuery);
+                    createTableQuery = "";
+                    count = 0;
+                }
+
+                if (count == 0)
+                {
+                    createTableQuery = titles.Aggregate(createTableQuery, (current, title) => current + ("[" + title + "], "));
+
+                    createTableQuery = createTableQuery.Trim(',', ' ');
+                    createTableQuery += ") VALUES (";
+                }
+
+                createTableQuery = (from string onecell in sheetValues[0] select onecell.Replace("'", " ")).
+                    Aggregate(createTableQuery, (current, temponecell) => current + ("'" + temponecell + "', "));
+
+                createTableQuery = createTableQuery.Trim(',', ' ');
+                createTableQuery += "),(";
+                createTableQuery = createTableQuery.Replace("(),", "");
+
+                sheetValues.RemoveAt(0);
+                count++;
+            }
+
+            createTableQuery = createTableQuery.Trim(',', '(');
+            eachThousandRows.Add(createTableQuery);
+
+            return eachThousandRows;
+        }
+    }
+}
